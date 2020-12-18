f8b4e9f0818c71d876d2fd74335827850b71323d
bonnysid <bonnysidworker@gmail.com>
2020-12-18 17:42:38 +0400

    index generator

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const DataGenerator = require('./DataGenerator');
 
-DataGenerator.start();
-
+// DataGenerator.start();
+DataGenerator.generateIndexes('Поставщик', 'Название_фирмы,Среднее_время_доставки,Имя_кл,Фамилия_кл,Отчество_кл,email,номер_телефона,количество_бракованных_товаров');
 
